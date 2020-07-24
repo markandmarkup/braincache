fb37e7f2fcc4104e6c5996a2cc9e4a832cc2f991
Mark Eyles <markeyles88@gmail.com>
2020-07-24 17:06:33 +0100

    First commit. Added User model, migration, and seed factory

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('username')->unique();
+            $table->string('email');
+            $table->string('password');
+            $table->string('icon');
+            $table->string('about_me');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('join_date')->now();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
